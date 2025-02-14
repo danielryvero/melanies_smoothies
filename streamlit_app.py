f041a6892a24eee35f8f7d71f9447547bc469d5d
Daniel Rivero <122742858+danielryvero@users.noreply.github.com>
2025-02-14 12:32:01 +0100

    changes for API

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,6 +18,11 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
+# New section to displau smoothiefroot nutrtion information
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
+
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:",
     my_dataframe,
@@ -44,7 +49,3 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, '+name_on_order, icon="✅")
 
-# New section to displau smoothiefroot nutrtion information
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
