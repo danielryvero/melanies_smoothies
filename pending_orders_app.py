466defcdaaa85d5d8c701e3a175302845a936a35
Daniel Rivero <122742858+danielryvero@users.noreply.github.com>
2025-08-01 14:01:13 +0200

    Update pending_orders_app.py

--- pending_orders_app.py
+++ pending_orders_app.py
@@ -1,56 +1,37 @@
-# Import python packages
-# Import python packages
 import streamlit as st
-from snowflake.snowpark.functions import col
+from snowflake.snowpark.functions import col, when_matched
 import requests
 import pandas as pd
 
-# Write directly to the app
 st.title(":cup_with_straw: Pending Smoothie Orders!:cup_with_straw:")
 st.write(
     """Orders that need to be filled.
     """
 )
 
-#name_on_order = st.text_input("Pending Orders:")
-
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-#session = get_active_session()
-#my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0)
 
-orders = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).to_pandas()
-#st.write("Columns in orders:", orders.columns.tolist())
+if my_dataframe:
+    editable_df = st.data_editor(my_dataframe, hide_index=True, use_container_width=True)
+    submitted = st.button('Submit')
 
-if orders.empty:
-    st.write("No pending orders")
+    
+    if submitted:
+
+        og_dataset = session.table("smoothies.public.orders")
+        edited_dataset = session.create_dataframe(editable_df)
+        
+        try:
+            og_dataset.merge(edited_dataset
+                         , (og_dataset['ORDER_UID'] == edited_dataset['ORDER_UID'])
+                         , [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
+                        )
+            st.success("Order(s) Updated!", icon = "👍")
+
+        except:
+            st.write('Something went wrong.')
 else:
-    editable_df=st.data_editor(orders, num_rows="dynamic")
-    if st.button("Submit"):
-        updated_orders=editable_df[
-            (editable_df["ORDER_FILLED"]==1)&(orders["ORDER_FILLED"]==0)
-        ]
-        if not updated_orders.empty:
-            for order_name in updated_orders["NAME_ON_ORDER"]:
-                update_query = f"""
-                UPDATE smoothies.public.orders
-                SET ORDER_FILLED = TRUE 
-                WHERE NAME_ON_ORDER = '{order_name}'
-                """
-                session.sql(update_query).collect()
-            st.success("Orders updated succesfully!", icon ="✅")
-            #st.write (update_query)
-        else:
-            st.warning("No changes detected", icon="⚠️")
-# try:
-#     og_dataset = session.table("smoothies.public.orders")
-#     edited_dataset = session.create_dataframe(orders)
-#     og_dataset.merge(edited_dataset
-#              , (og_dataset['ORDER_UID'] == edited_dataset['ORDER_UID'])
-#              , [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
-#             )
-#     st.success("Order(s) Updated!", icon = "👍")
-# except:
-#     st.write('Somwthing went wrong.')
+    st.success('There are no pending orders right now', icon = "👍")
